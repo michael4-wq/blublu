394f1f50fa97f90964e7c456dc89d87c5b11a8d9
micha <michaelkonovalov46@gmail.com>
2025-09-09 16:31:27 +0300

    commit mge

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from difflib import SequenceMatcher
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.enums import ParseMode
-
+import time
 import config
 import config_memes as cfg
 import buttons
